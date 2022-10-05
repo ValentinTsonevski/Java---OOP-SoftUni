ea61a827b4af51008aa5273c5b1f33a7c6b19e37
Valentin Tsonevski <tsonevski99@gmail.com>
2022-10-05 19:36:22 +0100

    Add files via upload

---
+++ Working with Abstraction - Exercise/src/CardRank/CardsRank.java
@@ -0,0 +1,17 @@
+package CardRank;
+
+public enum CardsRank {
+    ACE,
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE,
+    TEN,
+    JACK,
+    QUEEN,
+    KING;
+}
