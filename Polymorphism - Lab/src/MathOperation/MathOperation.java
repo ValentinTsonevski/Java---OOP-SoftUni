02ac08ae34de26bcf75724828f509d6c8daec5a4
Valentin Tsonevski <tsonevski99@gmail.com>
2022-10-12 00:19:11 +0100

    Add files via upload

---
+++ Polymorphism - Lab/src/MathOperation/MathOperation.java
@@ -0,0 +1,17 @@
+package MathOperation;
+
+public class MathOperation {
+
+    public  int add(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public  int add(int num1, int num2, int num3) {
+        return num1 + num2 + num3;
+    }
+
+    public  int add(int num1, int num2, int num3, int num4) {
+        return num1 + num2 + num3 + num4;
+    }
+
+}
